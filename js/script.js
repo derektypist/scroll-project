114b2b2434230db0f347d0c8d9691208d484bc39
Derek Dhammaloka <derek_dhammaloka@yahoo.com>
2021-05-25 15:21:54 +0000

    Added condition for scrollHeight at script.js

--- js/script.js
+++ js/script.js
@@ -36,5 +36,15 @@ const topLink = document.querySelector(".top-link");
 
 window.addEventListener("scroll", function() {
     const scrollHeight = window.pageYOffset;
+    const navHeight = navbar.getBoundingClientRect().height;
+    if (scrollHeight > navHeight) {
+        navbar.classList.add("fixed-nav");
+    } else {
+        navbar.classList.remove("fixed-nav");
+    }
+
+    if (scrollHeight > 500) {
+        topLink.classList.add("show-link");
+    }
 });
 
