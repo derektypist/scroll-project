82f9835f6d52246a3cff056a26a678002fd83022
Derek Dhammaloka <derek_dhammaloka@yahoo.com>
2021-05-25 14:50:01 +0000

    Added set date and close links at script.js

---
+++ js/script.js
@@ -0,0 +1,24 @@
+/*
+    Element.getBoundingClientRect() method returns the size of the element
+    and its position relative to the viewport.
+
+    pageYOffset is a read-only window property that returns the number of
+    pixels the document has been scrolled vertically.
+
+    slice extracts a section of the string without modifying original string.
+
+    offsetTop - A Number, representing the top position of the element in pixels.
+
+*/
+
+// Set Date
+const date = document.getElementById("date");
+date.innerHTML = new Date().getFullYear();
+
+// Close Links
+const navToggle = document.querySelector('.nav-toggle');
+const linksContainer = document.querySelector('.links-container');
+const links = document.querySelector('.links');
+
+
+
